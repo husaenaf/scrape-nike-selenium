f9559fd9fb8e408ece107d947d7ccef47487fcbd
Husaen al Fatah <husaenaf26@gmail.com>
2023-09-25 16:56:27 +0700

    add file and change other

--- nike-beautifulsoup.py
+++ nike-beautifulsoup.py
@@ -1,44 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 from unidecode import unidecode
+import base64
 
-
-# scraping data Boys Running Shoes
-html_doc = requests.get('https://www.nike.com/id/w/boys-running-shoes-37v7jz4413nzy7ok')
-soup = BeautifulSoup(html_doc.text, 'html.parser')
+# Scraping data Boys Running Shoes
+url = 'https://www.nike.com/id/w/boys-running-shoes-37v7jz4413nzy7ok'
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser')
 boysjordan = soup.find('div', 'product-grid__items css-hvew4t')
-titles = boysjordan.find_all('div', 'product-card__titles')
-kindcolours = boysjordan.find_all('button', 'product-card__colorway-btn')
-fixprices = boysjordan.find_all('div', 'product-card__price')
-links = boysjordan.find_all('div', 'product-card__body')
-link_images = boysjordan.find_all('div', 'wall-image-loader')
 
+for product in boysjordan.find_all('div', 'product-card'):
+    title = product.find('div', 'product-card__title').text.strip()
+    kindcolour = product.find('button', 'product-card__colorway-btn').find('div').text.strip()
+    fixprice = unidecode(product.find('div', 'product-card__price').find('div').find('div').text.strip())
+    link = product.find('div', 'product-card__body').find('a')['href']
+    link_image = product.find('div', 'wall-image-loader').find('img')['src']
 
-for title, kindcolour, fixprice, link, link_image in zip(titles, kindcolours, fixprices, links, link_images):
-    title = title.find('div').text
     print('\n' + title)
-    try:
-        kindcolour = kindcolour.find('div').text
-        print(kindcolour)
-    except:
-        kindcolour = ''
-    try:
-        fixprice = fixprice.find('div').find('div').text    # pencarian fixprices di dalamnya div dan dalamnya ada div lagi
-        # Menggunakan unidecode untuk menghilangkan karakter non-ASCII yang ada di file CSV seperti Â dan lainnya
-        fixprice = unidecode(fixprice)
-        print(fixprice)
-    except:
-        fixprice = ''
-    try:
-        link = link.find('a')['href']
-        print(link)
-    except:
-        link = ''
-    try:
-        link_image = link_image.find('img')['src']
-        print(link_image)
-    except:
-        link_image = ''
-
-
-
+    print(kindcolour)
+    print(fixprice)
+    print(link)
+    print(link_image)
--- nike-scrapy.py
+++ nike-scrapy.py
@@ -6,13 +6,26 @@ class nike(scrapy.Spider):
     start_urls = ["https://www.nike.com/id/w/mens-basketball-shoes-3glsmznik1zy7ok"]
 
     def parse(self, response):
-        # print(response.body)
-        name = response.css("#Tatum\\ 1\\ \\'Home\\ Team\\'\\ PF::text").extract_first()
-        category = response.css("#skip-to-products > div:nth-child(1) > div > figure > div > div:nth-child(1) > div > div.product-card__subtitle::text").extract_first()
-        kindcolour = response.css("#skip-to-products > div:nth-child(1) > div > figure > div > div.product-card__count-wrapper.false.false > div > button > div::text").extract_first()
-        price = response.css("#skip-to-products > div:nth-child(1) > div > figure > div > div.product-card__animation_wrapper > div > div > div > div::text").extract_first()
+        print(response.body)
+        for i in range(1, 27):
+            for shoes in response.css('#skip-to-products'):
 
-        print(name)
-        print(category)
-        print(kindcolour)
-        print(price)
+                raw_price = shoes.css('div:nth-child('+str(i)+') > div > figure > div > div.product-card__animation_wrapper > div > div > div > div::text').extract_first()
+                if raw_price:
+                    # Membersihkan karakter non-breaking space ('\xa0')
+                    cleaned_price = raw_price.replace('\xa0', ' ')
+                else:
+                    cleaned_price = None
+
+                yield {
+                    'name': shoes.css('div:nth-child('+str(i)+') > div > figure > div > div:nth-child(1) > div > div.product-card__title::text').extract_first(),
+                    'category': shoes.css('div:nth-child('+str(i)+') > div > figure > div > div:nth-child(1) > div > div.product-card__subtitle::text').extract_first(),
+                    'kindcolour': shoes.css('div:nth-child('+str(i)+') > div > figure > div > div.product-card__count-wrapper.false.false > div > button > div::text').extract_first(),
+                    'price': cleaned_price,
+                    'galery': shoes.css('div:nth-child('+str(i)+') > div > figure > a.product-card__img-link-overlay > div > img[src]::attr(src)').extract_first(),
+
+                    # contoh format jika menggunakan css selector
+                    # 'price' : shoes.css('div:nth-child('+str(i)+') > div > figure > div > div.product-card__animation_wrapper > div > div > div > div::text').extract_first(),
+                    # dontoh format jika menggunakan xpath
+                    # 'price': response.xpath('//*[@id="skip-to-products"]/div[1]/div/figure/div/div[3]/div/div/div/div/text()').extract_first()
+                }
